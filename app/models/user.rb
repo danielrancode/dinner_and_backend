024ac27e67f651efdc83a93ba295668f0b728c41
Daniel Ran <danielr1735@gmail.com>
2018-09-24 21:24:34 -0400

    minor things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, uniqueness: { case_sensitive: false }
   validates :username, uniqueness: { case_sensitive: false }
   has_many :programs
   has_many :events, through: :programs
